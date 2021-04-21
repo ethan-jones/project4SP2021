8d6821ea7829254208e7ca75dd356dbe95751fc7
dw3ght <78623643+dw3ght@users.noreply.github.com>
2021-04-20 23:07:38 -0400

    Update DwightTreeType.h

--- DwightTreeType.h
+++ DwightTreeType.h
@@ -1,21 +1,17 @@
 #include <string>
 #include <fstream>
 #include "QueType.h"
-
-//typedef int ItemType;
-template <class ItemType>
+typedef int ItemType;
 struct TreeNode;
-
+#include "QueType.cpp"
 enum OrderType {PRE_ORDER, IN_ORDER, POST_ORDER};
-
-template <class ItemType>
 class TreeType
 {
 public:
   TreeType();                     // constructor
   ~TreeType();                    // destructor
-  TreeType(const TreeType<ItemType>& originalTree); 
-  void operator=(const TreeType<ItemType>& originalTree); // copy constructor
+  TreeType(const TreeType& originalTree); 
+  void operator=(const TreeType& originalTree); // copy constructor
   void MakeEmpty();
   bool IsEmpty() const;
   bool IsFull() const;
@@ -25,21 +21,23 @@ public:
   void DeleteItem(ItemType item);
   void ResetTree(OrderType order); 
   ItemType GetNextItem(OrderType order, bool& finished);
-  void PreOrder(TreeNode<ItemType> * tree, QueType<ItemType> & que);
-  void PostOrder(TreeNode<ItemType> * tree, QueType<ItemType> & que);
-  void InOrder(TreeNode<ItemType> * tree, QueType<ItemType> & que);
-  std::string Print() const;
-  std::string PreOrderPrint();
-  std::string PostOrderPrint();
-  std::string InOrderPrint();
+  void Print() const;
+  void inOrderTraverse(TreeNode* tree);
   void LevelOrderPrint();
-  void PrintAncestors(ItemType value);
-  int  GetSuccessor(int value);
-  ItemType mirrorImage(TreeType& t);
+  void PrintAncestors(int value);
+  void GetSuccessor(TreeNode* tree, ItemType& data);
+  void mirrorImage(TreeType& t);
+  void PostOrderPrint() const;
+  void PreOrderPrint() const;
+  void Level(TreeNode* tree);
+  void CopyTree(TreeNode*& copy, const TreeNode* originalTree);
+  void PostOrder(TreeNode* tree, QueType<ItemType>& postQue);
+  int CountNodes(TreeNode* tree);
+  void Retrieve(TreeNode* tree, ItemType& item, bool& found);
 private:
-  TreeNode<ItemType> * root;
+  TreeNode* root;
   QueType<ItemType> preQue;
   QueType<ItemType> inQue;
   QueType<ItemType> postQue;
-  TreeNode<ItemType> * PtrToSuccessor(TreeNode<ItemType> * tree);
+  TreeNode* PtrToSuccessor(TreeNode* tree);
 };
